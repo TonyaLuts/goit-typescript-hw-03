95125bad2512eae9099cd9384b159a6b842a7f5e
TonyaLuts <131724958+TonyaLuts@users.noreply.github.com>
2024-01-04 15:27:34 +0000

    Ts-3

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,43 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number = Math.random()) {}
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: Boolean;
+  protected tenants: Person[] = [];
+  constructor(protected key: Key) {}
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
